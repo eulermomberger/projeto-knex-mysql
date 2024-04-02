d3de1afc5d794bd5e631265e9887345faaceab9d
Euler <mombergereuler@gmail.com>
2024-04-02 00:19:04 -0300

    feat: teste db

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const db = require('./db'); // Arquivo onde configuramos o Knex
+const db = require('./db'); // Arquivo onde configuramos o Knex
 
 const app = express();
 const PORT = process.env.PORT || 3000;
