98376b55b74f3e0b0670229312341ad71bc94959
Euler <mombergereuler@gmail.com>
2024-04-02 00:16:18 -0300

    feat: fazendo o railway ver minha aplicação

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const db = require('./db'); // Arquivo onde configuramos o Knex
+// const db = require('./db'); // Arquivo onde configuramos o Knex
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,32 +8,32 @@ app.use(express.json());
 app.use(express.raw({ type: 'application/vnd.custom-type' }));
 app.use(express.text({ type: 'text/html' }));
 
-// Rota para listar todos os usuários
-app.get('/users', async (req, res) => {
-  try {
-    const users = await db('users').select('*');
-    res.json({ users });
-  } catch (error) {
-    console.error('Erro ao listar usuários:', error);
-    res.status(500).json({ error: 'Erro interno do servidor' });
-  }
-});
+// // Rota para listar todos os usuários
+// app.get('/users', async (req, res) => {
+//   try {
+//     const users = await db('users').select('*');
+//     res.json({ users });
+//   } catch (error) {
+//     console.error('Erro ao listar usuários:', error);
+//     res.status(500).json({ error: 'Erro interno do servidor' });
+//   }
+// });
 
-// Rota para criar um novo usuário
-app.post('/users', async (req, res) => {
-  const { name, email } = req.body;
-  if (!name || !email) {
-    return res.status(400).json({ error: 'Nome e email são obrigatórios' });
-  }
+// // Rota para criar um novo usuário
+// app.post('/users', async (req, res) => {
+//   const { name, email } = req.body;
+//   if (!name || !email) {
+//     return res.status(400).json({ error: 'Nome e email são obrigatórios' });
+//   }
 
-  try {
-    await db('users').insert({ name, email });
-    res.status(201).json({ message: 'Usuário criado com sucesso' });
-  } catch (error) {
-    console.error('Erro ao criar usuário:', error);
-    res.status(500).json({ error: 'Erro interno do servidor' });
-  }
-});
+//   try {
+//     await db('users').insert({ name, email });
+//     res.status(201).json({ message: 'Usuário criado com sucesso' });
+//   } catch (error) {
+//     console.error('Erro ao criar usuário:', error);
+//     res.status(500).json({ error: 'Erro interno do servidor' });
+//   }
+// });
 
 app.get('/', async (req, res) => {
   res.send(`
