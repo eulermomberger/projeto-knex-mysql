8bedf1bbfe94d00b1e3759cef3948d611c5a672b
Euler <mombergereuler@gmail.com>
2024-04-01 22:44:41 -0300

    feat: utilizar variáveis de ambiente

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,13 @@
-// Update with your config settings.
+require('dotenv').config();
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host: '127.0.0.1',
-      user: 'root',
-      password: 'root',
-      database: 'knex_mysql'
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
     },
     migrations: {
       tableName: 'knex_migrations'
