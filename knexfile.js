d49b021a7cd19ee5b8730578deebb073daa0695e
Euler <mombergereuler@gmail.com>
2024-04-01 23:45:38 -0300

    chore: adicionando postgresql

--- knexfile.js
+++ knexfile.js
@@ -14,12 +14,15 @@ module.exports = {
     }
   },
   production: {
-    client: 'mysql',
+    client: 'pg',
     connection: {
-      host: process.env.MYSQL_HOST,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
+      connectionString: process.env.DATABASE_URL,
+      host: process.env.PG_HOST,
+      port: process.env.PG_PORT,
+      user: process.env.PG_USER,
+      database: process.env.PG_DATABASE,
+      password: process.env.PG_PASSWORD,
+      ssl: process.env.PG_SSL ? { rejectUnauthorized: false } : false,
     },
     migrations: {
       tableName: 'knex_migrations'
