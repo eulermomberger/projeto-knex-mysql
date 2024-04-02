77ad6775d9acdf56f762d0ca858395046759ea5a
Euler <mombergereuler@gmail.com>
2024-04-01 23:17:00 -0300

    chore: adicionando variaveis de ambiente no knex

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const knex = require('knex');
 const knexConfig = require('./knexfile');
 
-const db = knex(knexConfig.development);
+const db = knex(knexConfig[process.env.environment] || knexConfig.development);
 
 module.exports = db;
--- knexfile.js
+++ knexfile.js
@@ -12,5 +12,17 @@ module.exports = {
     migrations: {
       tableName: 'knex_migrations'
     }
+  },
+  production: {
+    client: 'mysql',
+    connection: {
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 };
